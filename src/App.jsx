a29d3f2266afc02bcdce0350d0f34e41cf7fcc43
deshinta <deshintaaulia065@gmail.com>
2024-09-27 20:56:53 +0700

    first setup project

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { RouterProvider } from "react-router-dom";
+import route from "./router";
+
+function App() {
+  return <RouterProvider router={route} />;
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1 className="text-xl text-center pt-5">Home Page</h1>;
+}
