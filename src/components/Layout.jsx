7604442ad3a9a63fbe96220d839d02e9a3442d80
deshinta <deshintaaulia065@gmail.com>
2024-09-30 16:57:45 +0700

    style: style layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -15,15 +15,32 @@ export default function LayoutMain() {
       />
       <div className="drawer-content">
         {/* Page content here */}
-        <div className="bg-[#56A0AF] text-white flex h-20 items-center gap-3 px-3">
-          <div
-            // className={`${openDrawer && "hidden"}`}
-            className="hover:bg-[#206F7E] rounded-full p-2"
-            onClick={() => setOpenDrawer(!openDrawer)}
-          >
-            <Bars3Icon className="w-6" />
+        <div className="bg-[#56A0AF] text-white flex justify-between h-20 items-center px-3">
+          <div className="flex items-center">
+            <div
+              // className={`${openDrawer && "hidden"}`}
+              className="hover:bg-[#206F7E] rounded-full p-2"
+              onClick={() => setOpenDrawer(!openDrawer)}
+            >
+              <Bars3Icon className="w-6" />
+            </div>
+            <h1 className="text-xl ml-3 font-bold">Lorem</h1>
           </div>
-          <h1 className="text-xl ml-3 font-bold">Lorem</h1>
+          <label className="input input-bordered flex items-center gap-2">
+            <input type="text" className="grow" placeholder="Search" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="h-4 w-4 opacity-70 text-slate-500"
+            >
+              <path
+                fillRule="evenodd"
+                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </label>
         </div>
         <Outlet />
       </div>
